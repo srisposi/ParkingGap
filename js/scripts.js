b7a14a38879b3eed67240b864ce6f8150e1cde11
srisposi <srisposi@iiconcepcion.edu.ar>
2021-07-11 20:22:46 -0300

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
   $('[data-toggle="popover"]').popover();
 
   $('[data-toggle="tooltip"]').tooltip();
-});
+}); */
+
+
+
+/* $('#titulo').animate({  left:'250px',
+                        opacity:'0.5',
+                        height:'150px',
+                        width:'150px'   },3000) */
+/*  
+$('titulo').animate({right:'250'},3000) */
